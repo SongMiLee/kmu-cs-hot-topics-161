32ac5c2100e799920bb555c539a1d9bb87771c6e
SongMiLee <s91010@naver.com>
2016-05-01 21:06:51 -0700

    replace special character

--- src/main/scala/scala-wordcount.scala
+++ src/main/scala/scala-wordcount.scala
@@ -1,32 +1,50 @@
+import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.feature.StopWordsRemover
 
 object WordCount {
   def main(args: Array[String]) {
-    val removeCharacter = Array()
-
     val conf = new SparkConf().setAppName("Word Count")
     val sc = new SparkContext(conf)
 
     print("input file location is: " + args(0))
-    val tweets = sc.textFile(args(0))
+    val tweets = sc.textFile(args(0), 12)
 
     val tweet_words = tweets.flatMap(x=>x.split("\\s+"))
+    val w = Array("`", "-", "=", ";", "'", "/", "~", "!", "@",
+		 "#", "$", "%", "^", "&", "|", ":", "<", ">",
+		 "\\*", "\\+","\\{","\\}","\\?",".")
 
-    val remover = new StopWordsRemover().setInputCol("raw").setOutputCol("filtered")
+    //Stop Words String Array
+    val remover = new StopWordsRemover().getStopWords
+    var tmp = tweet_words
+    var tmp_words = tmp
 
-    val len_words = tweet_words.filter(x => x.length()>4) // word length is bigger than 4
-    val lower_words = len_words.map(x=>x.toLowerCase()) //make words lower
+    //replace stop words to ""
+    for(i<- 0 to (remover.length-1)){
+      tmp_words = tmp.filter(x=>(!x.contains(remover(i),"")))
+      tmp = tmp_words
+    }
 
-    val kv = lower_words.map(x => (x, 1))
+  //replace special character
+    for(i<-0 to (w.length-1)){
+      tmp_words = tmp.map(x=>x.replace(w(i), ""))
+      tmp = tmp_words
+    }
+
+    val len = tmp.filter(x => x.length()>4) // word length is bigger than 4
+    val lower = len.map(x=>x.toLowerCase()) //make words lower
+
+    println(lower.take(10))
+
+    val kv = lower.map(x => (x, 1))
     val word_count = kv.reduceByKey((x,y) => x+y)
+
     val top_ten = word_count.takeOrdered(10)(Ordering[Int].reverse.on(x=>x._2))
     top_ten.foreach(println)
-/*
-    var file = new File(args(1))
-    var out = fileSystem.createNewFile(new Path(file.getName))
-    out.write(top_ten)
-    out.close()*/
+
+    val writeRDD = sc.parallelize(top_ten)
+    writeRDD.saveAsTextFile(args(1))
+
   }
 }
